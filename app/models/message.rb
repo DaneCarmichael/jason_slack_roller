e949f7d2a82db8fafb4c8a92e5369f3845b050df
Dane Carmichael <carmichaeldane@gmail.com>
2016-09-26 14:33:35 -0500

    Fixing where the three digit cut off is for more then 3 digit die

--- app/models/message.rb
+++ app/models/message.rb
@@ -3,7 +3,8 @@ class Message < ApplicationRecord
   def roll_dice
     if self.body[/\d{1,3}d\d{1,3}/]
       num_times = self.body[/\b\d{1,3}/].to_i
-      num_of_sides = self.body[/\d{1,3}\b/].to_i
+      num_of_sides = self.body[/d\d*/][/\d{1,3}/].to_i
+      binding.pry
       rolls = roll(num_times, num_of_sides)
       if self.body[/drop/]
         sorted = rolls.sort
