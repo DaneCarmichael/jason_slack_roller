edc3a1e5ee27b9c02881d9bad8ef52e2c40d6cec
Dane Carmichael <carmichaeldane@gmail.com>
2019-11-16 17:40:10 -0600

    Wild die works

--- app/models/message.rb
+++ app/models/message.rb
@@ -22,17 +22,26 @@ class Message < ApplicationRecord
 
   def roll_dice
     if self.body[/\d{1,3}d\d{1,3}/]
+      wild = []
       roll_params = parse_message
       roll_params = numberize_die(roll_params)
       rolls = roll(roll_params[:times_rolled],
                    roll_params[:sides_to_die])
       rolls, dropped = sorted_drop(rolls) if roll_params[:dropped_die]
+      if roll_params[:wild]
+        wild_die =  rolls.shift
+        if wild_die == 6
+          wild = roll_it_out(wild_die)
+        else
+          wild = [wild_die]
+        end
+      end
       if roll_params[:attachment]
         attach = Attachment.new(roll_params[:attachment][0],
                                 roll_params[:attachment][/\d{1,3}/])
-        self.body = build_roll_message(rolls, attach, dropped)
+        self.body = build_roll_message(rolls, attach, dropped, wild)
       else
-        self.body = build_roll_message(rolls, nil, dropped)
+        self.body = build_roll_message(rolls, nil, dropped, wild)
       end
     else
       self.body = build_roll_message(roll)
@@ -55,6 +64,16 @@ class Message < ApplicationRecord
     num_times.times.collect { return_die_result(sides) }
   end
 
+  def roll_it_out(wild_die, wild_dice = [])
+    wild_dice.push(wild_die)
+    die = roll(1, 6).first
+    if die == 6
+      roll_it_out(die, wild_dice)
+    else
+      return wild_dice.push(die)
+    end
+  end
+
   def sorted_drop(rolls)
     rolls.sort!
     if self.body.downcase[/high/]
@@ -65,18 +84,26 @@ class Message < ApplicationRecord
     [rolls, dropped]
   end
 
-  def build_roll_message(rolls, attach = nil, dropped = nil)
+  def wild_die(die)
+  end
+
+  def build_roll_message(rolls, attach = nil, dropped = nil, wild)
+    rolls = wild.empty? ? roll : (wild + rolls)
     total = rolls.sum
     if attach
       total = total.public_send(attach.op, attach.mod)
     end
     "#{self.user_name} rolls #{self.body}, resulting in"\
     " *#{rolls.join(", ")}* for a total of"\
-    " *#{total}* #{dropped_message(dropped)}"
+    " *#{total}* #{dropped_message(dropped)} #{wild_message(wild)}"
   end
 
   def dropped_message(dropped = nil)
     " _dropped #{dropped}_" if dropped
+  end
+
+  def wild_message(wild = nil)
+    "Wild die results: #{wild.join(", ")}" if wild
   end
 
   def return_die_result(sides)
